dbdc9bbdc6d2d77f51de1f7ae93b71e30a35388c
thamara <thamara@nfe>
2016-10-12 15:05:22 -0300

    Add header for crypto interface

---
+++ src/crypto_interface.h
@@ -0,0 +1,29 @@
+/* Copyright (c) 2016 Pablo G. Gallardo <pggllrd@gmail.com>
+ *
+ * This file is part of LivreNFE.
+ *
+ * LivreNFE is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * LivreNFE is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with LivreNFE.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef	CRYPTO_I_H
+#define	CRYPTO_I_H
+
+extern int encrypt(char *in, char **out);
+
+extern int decrypt(char *in, char **out);
+
+extern int sign(char *in, char **out);
+
+#endif
