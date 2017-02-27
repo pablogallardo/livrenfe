247fc6f4e861a1fbb64d91d6eb398b81d1ff1a38
Pablo G. Gallardo <pggllrd@gmail.com>
2017-02-27 14:00:31 -0300

    Fix typo

--- src/db_nfe.c
+++ src/db_nfe.c
@@ -4,8 +4,9 @@
  *
  * LivreNFE is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or * (at your option) any later version.
- * * LivreNFE is distributed in the hope that it will be useful,
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * LivreNFE is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
